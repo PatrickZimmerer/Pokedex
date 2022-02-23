352941d50485713339849b73c9ebe58984782ebc
Patrick Zimmerer <patrick.zimmerer1@gmail.com>
2022-02-23 08:32:22 +0100

    styled Containers

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
-let currentPokemon;
-let currentPokemonImg;
-
-/* async function loadPokemon(){
-    let url = `https://pokeapi.co/api/v2/pokemon/charizard`;
-    let response = await fetch(url);
-    let currentPokemon = await response.json();
-    let currentPokemonType = currentPokemon['types'][0]['type']['name'];
-    let currentPokemonImg = currentPokemon['sprites']['other']['home']['front_default'];
-    renderPokemonInfo(currentPokemon, currentPokemonImg, currentPokemonType);
-} */
+function getById(id){
+    return document.getElementById(id);
+}
 async function loadPokemon(){
-    for (let i = 1; i < 152; i++) {
+    for (let i = 1; i < 22; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         let responseAsJson = await response.json();
-        showPokemon(responseAsJson);
+        showPokemon(responseAsJson, i);
         //console.log(responseAsJson);
     }
 }
 
-function showPokemon(responseAsJson){
+function showPokemon(responseAsJson, i){
     let PokemonId = responseAsJson['id']
     let PokemonName = responseAsJson['name'];
+    console.log(responseAsJson['types'])
     let PokemonType = responseAsJson['types'][0]['type']['name'];
+    //let PokemonTypeSecond = responseAsJson['types'][1]['type']['name'];
     let PokemonImg = responseAsJson['sprites']['other']['home']['front_default'];    
-    console.log(PokemonName, PokemonId, PokemonType, PokemonImg);
-    renderPokemon(PokemonId, PokemonName, PokemonType, PokemonImg);
+    renderPokemon(PokemonId, PokemonName, PokemonType, PokemonImg, i);
 }
 
-function renderPokemon(PokemonId, PokemonName, PokemonType, PokemonImg){
-    document.getElementById('pokeWrap').innerHTML += `<!--html-->
-    <div class="pokeContainer red">
+function renderPokemon(PokemonId, PokemonName, PokemonType ,PokemonImg, i){
+    getById('pokeWrap').innerHTML += `
+    <div id='container-${i}' class="pokeContainer">
         <div class="pokeId">
-            <p> #${PokemonId} </p> 
+            <p id="id-${i}" class="id"> #${PokemonId} </p> 
         </div>
         
         <div class="pokeNameTypeContainer">
             <h2 class="ucwords" class="pokeName">
                 ${PokemonName} 
                 </h2> 
-            <span class="badge rounded-pill bg-danger"> 
+            <span id="type-1-${i}" class="badge rounded-pill"> 
                 <p class="pokeType ucwords">
                     ${PokemonType}
                 </p>
-            </span>  
+            </span> 
+           
         </div>       
              
         <div class="pokeImgContainer">
@@ -51,6 +45,63 @@ function renderPokemon(PokemonId, PokemonName, PokemonType, PokemonImg){
         </div>
     </div
     `;
+    applyBackgrounds(PokemonType, i);
+}
+function applyBackgrounds(PokemonType, i){
+    console.log(PokemonType);
+    if (PokemonType == 'grass') {
+        getById('container-'+i).classList.add('green');
+        getById('id-'+i).classList.add('green-side');
+        getById('type-1-'+i).classList.add('bg-success');
+        //getById('type-2-'+i).classList.add('bg-success');
+    }
+    if (PokemonType == 'fire') {
+        getById('container-'+i).classList.add('red');
+        getById('id-'+i).classList.add('red-side');
+        getById('type-1-'+i).classList.add('bg-red-pill');
+    }
+    if (PokemonType == 'water') {
+        getById('container-'+i).classList.add('blue');
+        getById('id-'+i).classList.add('blue-side');
+        getById('type-1-'+i).classList.add('bg-blue-pill');
+    }
+    if (PokemonType == 'electric') {
+        getById('container-'+i).classList.add('yellow');
+        getById('id-'+i).classList.add('yellow-side');
+        getById('type-1-'+i).classList.add('bg-warning');
+    }
+    if (PokemonType == 'normal') {
+        getById('container-'+i).classList.add('brown');
+        getById('id-'+i).classList.add('brown-side');
+        getById('type-1-'+i).classList.add('bg-brown-pill');
+    }
+    if (PokemonType == 'psychic') {
+        getById('container-'+i).classList.add('purple');
+        getById('id-'+i).classList.add('purple-side');
+        getById('type-1-'+i).classList.add('bg-purple-pill');
+    }
+    if (PokemonType == 'flight') {
+        getById('container-'+i).classList.add('white');
+        getById('id-'+i).classList.add('white-side');
+        getById('type-1-'+i).classList.add('bg-white-pill');
+    }
+    if (PokemonType == 'fairy') {
+        getById('container-'+i).classList.add('pink');
+        getById('id-'+i).classList.add('pink-side');
+        getById('type-1-'+i).classList.add('bg-pink-pill');
+    }
+    if (PokemonType == 'bug') {
+        getById('container-'+i).classList.add('green');
+        getById('id-'+i).classList.add('green-side');
+        getById('type-1-'+i).classList.add('bg-success');
+    }
 }
 
-
+/*
+, PokemonTypeSecond
+<span id="type-2-${i}" class="badge rounded-pill">
+<p class="pokeType ucwords">
+    ${PokemonTypeSecond}
+</p>
+</span>  
+*/
