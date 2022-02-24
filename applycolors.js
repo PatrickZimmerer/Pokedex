6049293724c0b6f108b647b1129a4da2edc6a725
Patrick Zimmerer <patrick.zimmerer1@gmail.com>
2022-02-24 12:32:57 +0100

    added showDetails+sounds

--- applycolors.js
+++ applycolors.js
@@ -1,105 +1,16 @@
-function applyBackgrounds(responseAsJson, PokemonType, i){
-    console.log(PokemonType);
-    if (PokemonType == 'grass') {
+function applyBackgrounds(responseAsJson,PokemonType, i){
+    if (PokemonType) {
         getById('container-'+i).classList.add(`${PokemonType}`);
-        getById('id-'+i).classList.add('grass-side');
-        getById('type-1-'+i).classList.add('bg-grass-pill');
-        //getById('type-2-'+i).classList.add('bg-success');
-    }
-    if (PokemonType == 'fire') {
-        getById('container-'+i).classList.add('fire');
-        getById('id-'+i).classList.add('fire-side');
-        getById('type-1-'+i).classList.add('bg-fire-pill');
-    }
-    if (PokemonType == 'water') {
-        getById('container-'+i).classList.add('water');
-        getById('id-'+i).classList.add('water-side');
-        getById('type-1-'+i).classList.add('bg-water-pill');
-    }
-    if (PokemonType == 'electric') {
-        getById('container-'+i).classList.add('electric');
-        getById('id-'+i).classList.add('electric-side');
-        getById('type-1-'+i).classList.add('bg-electric-pill');
-    }
-    if (PokemonType == 'normal') {
-        getById('container-'+i).classList.add('normal');
-        getById('id-'+i).classList.add('normal-side');
-        getById('type-1-'+i).classList.add('bg-normal-pill');
-    }
-    if (PokemonType == 'bug') {
-        getById('container-'+i).classList.add('bug');
-        getById('id-'+i).classList.add('bug-side');
-        getById('type-1-'+i).classList.add('bg-bug-pill');
-    }    
-    if (PokemonType == 'ice') {
-        getById('container-'+i).classList.add('ice');
-        getById('id-'+i).classList.add('ice-side');
-        getById('type-1-'+i).classList.add('bg-ice-pill');
-    }
-    if (PokemonType == 'fighting') {
-        getById('container-'+i).classList.add('fighting');
-        getById('id-'+i).classList.add('fighting-side');
-        getById('type-1-'+i).classList.add('bg-fighting-pill');
-    }
-    if (PokemonType == 'ground') {
-        getById('container-'+i).classList.add('ground');
-        getById('id-'+i).classList.add('ground-side');
-        getById('type-1-'+i).classList.add('bg-ground-pill');
-    }
-    if (PokemonType == 'rock') {
-        getById('container-'+i).classList.add('rock');
-        getById('id-'+i).classList.add('rock-side');
-        getById('type-1-'+i).classList.add('bg-rock-pill');
-    }
-    if (PokemonType == 'dark') {
-        getById('container-'+i).classList.add('dark');
-        getById('id-'+i).classList.add('dark-side');
-        getById('type-1-'+i).classList.add('bg-dark-pill');
-    }
-    if (PokemonType == 'poison') {
-        getById('container-'+i).classList.add('poison');
-        getById('id-'+i).classList.add('poison-side');
-        getById('type-1-'+i).classList.add('bg-poison-pill');
-    }
-    if (PokemonType == 'psychic') {
-        getById('container-'+i).classList.add('psychic');
-        getById('id-'+i).classList.add('psychic-side');
-        getById('type-1-'+i).classList.add('bg-psychic-pill');
-    }
-    if (PokemonType == 'fairy') {
-        getById('container-'+i).classList.add('fairy');
-        getById('id-'+i).classList.add('fairy-side');
-        getById('type-1-'+i).classList.add('bg-fairy-pill');
-    }
-    if (PokemonType == 'ghost') {
-        getById('container-'+i).classList.add('ghost');
-        getById('id-'+i).classList.add('ghost-side');
-        getById('type-1-'+i).classList.add('bg-ghost-pill');
-    }
-    if (PokemonType == 'dragon') {
-        getById('container-'+i).classList.add('ghost');
-        getById('id-'+i).classList.add('ghost-side');
-        getById('type-1-'+i).classList.add('bg-ghost-pill');
-    }
-    if (PokemonType == 'steel') {
-        getById('container-'+i).classList.add('steel');
-        getById('id-'+i).classList.add('steel-side');
-        getById('type-1-'+i).classList.add('bg-steel-pill');
-    }
-    if (PokemonType == 'flying') {
-        getById('container-'+i).classList.add('flying');
-        getById('id-'+i).classList.add('flying-side');
-        getById('type-1-'+i).classList.add('bg-flying-pill');
+        getById('id-'+i).classList.add(`${PokemonType}-side`);
+        getById('type-1-'+i).classList.add(`bg-${PokemonType}-pill`);
     } 
     checkSecondType(responseAsJson, i);  
 }
 function checkSecondType(responseAsJson, i){
     let secondType = responseAsJson['types'][1];
     if (secondType){
-        console.log("true");
         applySecondType(responseAsJson, i)
     }
-
 }
 function applySecondType(responseAsJson, i){
     let secondPokemonType = responseAsJson['types'][1]['type']['name'];
@@ -109,4 +20,52 @@ function applySecondType(responseAsJson, i){
         ${secondPokemonType}
     </p> 
     `;
+}
+function applyDetailsBackgrounds(responseAsJson, PokemonType) {
+    getById('pokeWrapBig').classList.add(`${PokemonType}`);
+    getById('id').classList.add(`${PokemonType}-side`);
+    getById('type1').classList.add(`bg-${PokemonType}-pill`);
+    let secondType = responseAsJson['types'][1];
+    if (secondType){
+        applyDetailsSecondType(responseAsJson)
+    }
+}
+function applyDetailsSecondType(responseAsJson){
+    let secondPokemonType = responseAsJson['types'][1]['type']['name'];
+    getById('type2').classList.add('badge',`bg-${secondPokemonType}-pill`,'rounded-pill');
+    getById('type2').innerHTML += `
+    <p class="pokeTypeBig ucwords">
+        ${secondPokemonType}
+    </p> 
+    `;
+}
+async function backToAll(i){;
+    PLINK_AUDIO.play();
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+    let PokemonType = responseAsJson['types'][0]['type']['name'];
+    getById("pokedexWrap").classList.remove("d-none");
+    getById('pokeWrapBig').classList.remove(`${PokemonType}`);
+    getById('id').classList.remove(`${PokemonType}-side`);
+    getById('type1').classList.remove(`bg-${PokemonType}-pill`);
+    getById('pokeWrapBig').classList.add("d-none");  
+}
+function showAbout(){
+    PLINK_AUDIO.play();
+    getById('stats').classList.add('d-none');
+    getById('moves').classList.add('d-none');
+    getById('about').classList.remove('d-none');
+}
+function showStats(){
+    PLINK_AUDIO.play();
+    getById('stats').classList.remove('d-none');
+    getById('moves').classList.add('d-none');
+    getById('about').classList.add('d-none');
+}
+function showMoves(){
+    PLINK_AUDIO.play();
+    getById('stats').classList.add('d-none');
+    getById('moves').classList.remove('d-none');
+    getById('about').classList.add('d-none');
 }
--- script.js
+++ script.js
@@ -1,26 +1,27 @@
+const LVLUP_AUDIO = new Audio('./sounds/pokelvlup.mp3');
+const PLINK_AUDIO = new Audio('./sounds/pokeplink.mp3');
+PLINK_AUDIO.volume = 0.2;
 let atTheBottom = 0;
+
 
 function getById(id){
     return document.getElementById(id);
 }
 
-
 window.onscroll = async function loadMorePokemon() {
     if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
         atTheBottom++;
-        console.log(atTheBottom);
         let j = 1 + (21 * atTheBottom);
-        console.log(j);
         let k = 22 + (21 * atTheBottom);
-        console.log(k);
         for (let i = j; i < k; i++) {      
             let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
             let response = await fetch(url);
             let responseAsJson = await response.json();
             showPokemon(responseAsJson, i);
-     }
+        }
     }
 }
+
 async function loadPokemon(){
     for (let i = 1; i < 22; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
@@ -29,20 +30,19 @@ async function loadPokemon(){
         showPokemon(responseAsJson, i);
     }
 }
+
 function showPokemon(responseAsJson, i){
+    //console.log(responseAsJson)
     let PokemonId = responseAsJson['id']
     let PokemonName = responseAsJson['name'];
-    console.log(responseAsJson)
     let PokemonType = responseAsJson['types'][0]['type']['name'];
-    //let PokemonTypeSecond = responseAsJson['types'][1]['type']['name'];
     let PokemonImg = responseAsJson['sprites']['other']['home']['front_default'];    
     renderPokemon(responseAsJson, PokemonId, PokemonName, PokemonType, PokemonImg, i);
 }
+
 function renderPokemon(responseAsJson, PokemonId, PokemonName, PokemonType ,PokemonImg, i){
     getById('pokeWrap').innerHTML += ` 
-    <div onclick="showDetails(${responseAsJson}${i}${PokemonId}
-        ${PokemonName}${PokemonType}${PokemonImg})" 
-    id='container-${i}' class="pokeContainer">
+    <div onclick="showDetails(${i})" id='container-${i}' class="pokeContainer">
         <div class="pokeId">
             <p id="id-${i}" class="id"> #${PokemonId} </p> 
         </div>
@@ -71,16 +71,138 @@ function renderPokemon(responseAsJson, PokemonId, PokemonName, PokemonType ,Poke
     </div
     `;
     applyBackgrounds(responseAsJson, PokemonType, i);
-    /*checkSecondType(responseAsJson, i); */ 
 }
-/*
-function showDetails(responseAsJson, PokemonId, PokemonName, PokemonType ,PokemonImg, i){
+
+async function showDetails(i){
+    PLINK_AUDIO.play();
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+    let PokemonId = responseAsJson['id']
+    let PokemonName = responseAsJson['name'];
+    let PokemonType = responseAsJson['types'][0]['type']['name'];
+    let PokemonImg = responseAsJson['sprites']['other']['home']['front_default'];
     let PokemonHeight = responseAsJson['height'];
     let PokemonWeight = responseAsJson['weight'];
-    let PokemonBaseAbilities = responseAsJson['ability'][j]['ability']['name'];
+    //let PokemonBaseAbilities = responseAsJson['ability'][j]['ability']['name'];
     let PokemonBaseExp = responseAsJson['base_experience'];
-    let PokemonStats = responseAsJson['stats'][k]['base_stat'];
-    let PokemonMoves = responseAsJson['moves'][l]['move']['name'];
+    //let PokemonStats = responseAsJson['stats'][k]['base_stat'];
+    //let PokemonMoves = responseAsJson['moves'][l]['move']['name'];
+    getById("pokedexWrap").classList.add("d-none");
+    getById('pokeWrapBig').classList.remove('d-none');
+    getById('pokeWrapBig').innerHTML = `
+    <div class="pokeHeadBig">
+        <div>
+            <img onclick="backToAll(${i})" src="./img/arrow_back_white_36dp.svg">
+        </div>
+        <div>
+            <img id="like" src="./img/favorite_border_white_36dp.svg">
+        </div>
+    </div>    
 
+    <div class="pokeBodyBig">
+        <div class="pokeBodyContainerBig">
+            <div class="pokeNameTypeContainerBig">
+                <div>
+                <h2 class="ucwords h2Big" class="pokeNameBig">
+                    ${PokemonName} 
+                </h2> 
+                </div>
+                <div class="spanWrap">
+                <span id="type1" class="badge rounded-pill"> 
+                <p class="pokeTypeBig ucwords">
+                    ${PokemonType}
+                </p>
+                </span> 
+                <span id="type2">
+                    
+                </span>
+                </div>
+            </div>  
+
+            <div class="pokeIdBig">
+                <p id="id" class="idBig"> #${PokemonId} </p> 
+            </div>
+    </div>
+    <div class="pokeImgContainerBig">
+        <img src="${PokemonImg}" class="pokeImgBig" >  
+    </div>
+    </div>
+    <div class="pokeFootBig">
+        <div class="pokeFootLinks">
+            
+            <h3 onclick="showAbout()">About</h3>
+            <h3 onclick="showStats()">Base Stats</h3>
+            <h3 onclick="showMoves()">Moves</h3>
+
+        </div>
+
+        <div class="aboutWrap" id="about">
+            <div class="about">
+                <p>Main Type</p>
+                <p>Height</p>
+                <p>Weight</p>
+                <p>Abilities</p>
+                <p>Base Experience</p>
+            </div>
+            <div class="aboutDetails">
+                <p>${PokemonType}</p>
+                <p>${PokemonHeight} M</p>
+                <p>${PokemonWeight} KG</p>
+                <p>$PokemonBaseAbilities</p>
+                <p>${PokemonBaseExp}</p>
+            </div>
+        </div>
+
+        <div class="stats-wrap  d-none" id="stats">
+            <div class="statsList">
+                <p>HP</p>
+                <p>Attack</p>
+                <p>Defense</p>
+                <p>Sp. Atk</p>
+                <p>Sp. Def</p>
+                <p>Speed</p>
+            </div>
+            <div class="statsDetails">
+                <p>PokemonHp</p>
+                <p>PokemonAttack</p>
+                <p>PokemonDefense</p>
+                <p>PokemonSpAtk</p>
+                <p>PokemonSpDef</p>
+                <p>PokemonSpeed</p>
+            </div>
+            <div class="statsProgress">
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="movesWrap d-none" id="moves">
+            <h1>Moves</h1>
+            <div class="movesList">
+                <p class="badge rounded-pill bg-dark">PokemonAttacks</p>
+                
+            </div>
+        </div>
+        
+    </div>
+    `;
+    window.scrollTo(0, 0)
+    applyDetailsBackgrounds(responseAsJson, PokemonType);
 }
-*/
